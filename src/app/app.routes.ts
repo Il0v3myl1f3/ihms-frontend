82066a0bdb9c2e4e5cb5808358c1156eb9fe5b69
Vik92MD <114600692+Vik92MD@users.noreply.github.com>
2026-02-25 17:52:04 +0200

    feat: Add About route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ServicesComponent } from './components/pages/services/services.component';
 import { DoctorsComponent } from './components/pages/doctors/doctors.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'doctors', component: DoctorsComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ];
