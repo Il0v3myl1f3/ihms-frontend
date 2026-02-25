e78de13de604adb44ffbcac449cd862a85168caf
Oxani Dorin <caktus2005@gmail.com>
2026-02-25 17:21:13 +0200

    feat: Add update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ServicesComponent } from './components/pages/services/services.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
   { path: '**', redirectTo: '' }
 ];
