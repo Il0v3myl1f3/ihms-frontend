5e372cdb060ef35c4bbc437bf3879ed78c39ed4a
Il0v3myl1f3 <alexandrutrohin15@gmail.com>
2026-03-25 22:08:11 +0200

    feat: adjust doctor cards grid layout and remove social icons

--- src/app/components/pages/doctors/doctors.component.ts
+++ src/app/components/pages/doctors/doctors.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit, inject, ChangeDetectionStrategy } from '@angular/core';
-import { LucideAngularModule, Facebook, Twitter, Linkedin } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
+import { NgOptimizedImage } from '@angular/common';
 import { PageHeaderComponent } from '../../shared/page-header/page-header.component';
 import { MedicalService, Doctor } from '../../../services/medical.service';
 
 @Component({
   selector: 'app-doctors',
-  imports: [LucideAngularModule, PageHeaderComponent],
+  imports: [LucideAngularModule, PageHeaderComponent, NgOptimizedImage],
   templateUrl: './doctors.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DoctorsComponent implements OnInit {
-  readonly Facebook = Facebook;
-  readonly Twitter = Twitter;
-  readonly Linkedin = Linkedin;
+
 
   doctors: Doctor[] = [];
 
